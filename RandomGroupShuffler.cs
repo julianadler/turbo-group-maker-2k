74c2fd97448167ed79831ff465b4ee1a754522bc
Julian Adler <julian.adler@thldigital.com>
2022-08-30 19:43:24 +1200

    Tweaks and comments

--- RandomGroupShuffler.cs
+++ RandomGroupShuffler.cs
@@ -19,7 +19,11 @@ namespace TurboGroupMaker2K
             while (shift + GroupSize <= _names.Count)
             {
                 var index = 0;
+
+                // We find the most recent employee of the batch
                 string first = _names.Reverse().Skip(shift).First();
+
+                // We create random groups of two of the remaining employees
                 List<List<string>> groups = _names.Reverse()
                     .Skip(shift + 1)
                     .OrderBy(_ => _random.Next())
@@ -29,7 +33,10 @@ namespace TurboGroupMaker2K
 
                 foreach (List<string> group in groups)
                 {
+                    // We add the recent employee to the group
                     group.Insert(0, first);
+
+                    // We "finish" the group and make sure we are not returning a duplicate
                     if (TryFillGroupAndReturnIfNeeded(group))
                     {
                         yield return string.Join(", ", group);
@@ -40,36 +47,48 @@ namespace TurboGroupMaker2K
             }
         }
 
-        private bool TryFillGroupAndReturnIfNeeded(List<string> group,
-            bool tryFill = true)
+        private bool TryFillGroupAndReturnIfNeeded(List<string> group)
         {
             int hashCode = CalculateHashCode(group);
+
+            // If the group is 3 people and has not been generated before
             if (group.Count == GroupSize && _knownGroups.Add(hashCode))
             {
+                // We allow returning it
                 return true;
             }
 
-            if (tryFill && group.Count != GroupSize)
+            // if the group is incomplete
+            if (group.Count == GroupSize - 1)
             {
-                List<string> copy = group.ToList();
-                copy.Add(string.Empty);
                 foreach (string name in _names.OrderBy(_ => _random.Next()))
                 {
-                    copy[GroupSize - 1] = name;
-                    if (TryFillGroupAndReturnIfNeeded(copy, false))
+                    // Copy it with an empty third member
+                    var copy = new List<string>(group)
                     {
+                        name
+                    };
+
+                    // If the newly build group is satisfactory
+                    if (TryFillGroupAndReturnIfNeeded(copy))
+                    {
+                        // We replace the group passed as a parameter
                         group.Clear();
                         group.AddRange(copy);
+
+                        // This is now a valid group
                         return true;
                     }
                 }
             }
 
+            // The group already exists
+            // And we cannot create a new group from it
             return false;
         }
 
         /// <summary>
-        /// Combine the team member names and calculate a unique enough hash
+        /// Combine the group member names and calculate a unique enough hash
         /// </summary>
         private static int CalculateHashCode(List<string> grouping)
         {
